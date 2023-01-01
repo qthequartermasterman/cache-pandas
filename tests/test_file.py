68d02fb7c61dc95ccdb831403eb5b0d555b55564
qthequartermasterman <qthequartermasterman@gmail.com>
2023-01-01 11:34:20 -0600

    feat:memory caching with expiration time

--- tests/test_file.py
+++ tests/test_file.py
@@ -1,12 +1,16 @@
+import datetime
 import unittest
 from unittest import mock
 
+import freezegun
 import pandas as pd
 
 from cache_pandas.file import cache_to_csv
 
 NUM_SAMPLES = 10
 number_of_times_called = 0
+
+DUMMY_TIME = datetime.datetime(2012, 1, 1)
 
 
 def sample_constant_function() -> pd.DataFrame:
@@ -46,6 +50,39 @@ class TestCacheToCSV(unittest.TestCase):
 
         actual_df = wrapped_func()
         expected_df = sample_constant_function()
+
+        pd.testing.assert_frame_equal(actual_df, expected_df)
+        mock_csv.assert_called_once_with(self.filepath)
+
+    @freezegun.freeze_time(DUMMY_TIME)
+    @mock.patch("os.path.getmtime", return_value=DUMMY_TIME.timestamp())
+    @mock.patch("pandas.DataFrame.to_csv")
+    def test_does_not_cache_file_if_not_expired(self, mock_csv: mock.MagicMock, mock_getmtime: mock.MagicMock) -> None:
+        refresh_time = 100
+
+        expected_df = sample_constant_function()
+        with mock.patch("pandas.read_csv", return_value=expected_df):
+            wrapped_func = cache_to_csv(self.filepath, refresh_time=refresh_time)(sample_constant_function)
+
+            actual_df = wrapped_func()
+
+        pd.testing.assert_frame_equal(actual_df, expected_df)
+        mock_csv.assert_not_called()
+
+    @freezegun.freeze_time(DUMMY_TIME, as_kwarg="frozen_time")
+    @mock.patch("os.path.getmtime", return_value=DUMMY_TIME.timestamp())
+    @mock.patch("pandas.DataFrame.to_csv")
+    def test_caches_file_if_expired(self, mock_csv: mock.MagicMock, mock_getmtime: mock.MagicMock,
+                                    frozen_time=None, ) -> None:
+        refresh_time = 100
+        expiration_time = DUMMY_TIME + datetime.timedelta(seconds=refresh_time)
+
+        expected_df = sample_constant_function()
+
+        frozen_time.move_to(expiration_time)
+        with mock.patch("pandas.read_csv", return_value=expected_df):
+            wrapped_func = cache_to_csv(self.filepath, refresh_time=refresh_time)(sample_constant_function)
+            actual_df = wrapped_func()
 
         pd.testing.assert_frame_equal(actual_df, expected_df)
         mock_csv.assert_called_once_with(self.filepath)
